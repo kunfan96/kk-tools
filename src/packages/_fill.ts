b964fcb4dd25b0ecba557e0e6e2e9d0702bec7e5
kunfan96 <2622572046@qq.com>
2020-08-17 00:35:10 +0800

    test: typescript type

--- src/packages/_fill.ts
+++ src/packages/_fill.ts
@@ -3,7 +3,7 @@
  * @params value any
  * 
  */
-const _fill = (array, value, start, end) => {
+const _fill = (array: Array<any>, value: Number = 0, start:Number = 0, end:Number = 0) => {
         console.log(`${array}`)
 }
 
