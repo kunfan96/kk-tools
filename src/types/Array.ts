fd72ee2a77437a5b6e7dfb06783d8696e4cac0fb
KK1996 <15876870780@163.com>
2020-08-22 22:44:05 +0800

    fix

--- src/types/Array.ts
+++ src/types/Array.ts
@@ -1,3 +1,8 @@
+/**
+ * @module: 
+ * @func:The the collection about type
+*/
+
 export interface Ifill {
     <T, F>(initialValue: Array<T>, value: F, startIndex?:number, endIndex?:number):Array<T | F>
 }
