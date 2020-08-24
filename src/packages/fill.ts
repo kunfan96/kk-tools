dfe613f03347b3d647b47f527f66300373e6cdd7
KK1996 <15876870780@163.com>
2020-08-24 21:39:14 +0800

    fix:lint

--- src/packages/fill.ts
+++ src/packages/fill.ts
@@ -2,20 +2,26 @@
  * @name: fill
  * @function: fill values by specify position in Array
  * @extends: Array
- * 
+ *
  * @param initialValue Array initialed Array
  * @param value any filled value
  * @param startIndex Number filled started index
  * @param endIndex filled Number end index
  * @return Array
  */
-import {Ifill} from '../types/Array'
+import { Ifill } from "../types/Array"
 
-const fill:Ifill = <T, F>(initialValue: Array<T>, value: F, startIndex = 0, endIndex = initialValue.length - 1): Array<T|F>  => {    
+const fill: Ifill = <T, F>(
+    initialValue: Array<T>,
+    value: F,
+    startIndex = 0,
+    endIndex = initialValue.length - 1
+): Array<T | F> => {
     if (startIndex > endIndex) endIndex = startIndex
 
     /*----------------------------------------------*/
-    if (startIndex >= initialValue.length) endIndex = startIndex = initialValue.length - 1
+    if (startIndex >= initialValue.length)
+        endIndex = startIndex = initialValue.length - 1
 
     if (endIndex >= initialValue.length) endIndex = initialValue.length - 1
     /*----------------------------------------------*/
@@ -24,9 +30,11 @@ const fill:Ifill = <T, F>(initialValue: Array<T>, value: F, startIndex = 0, endI
     if (endIndex < 0) startIndex = endIndex = 0
 
     /*----------------------------------------------*/
-    let replaceValue:Array<T | F> = []
-    initialValue.forEach((item:T, index) => {
-        replaceValue.push((index >= startIndex && index <= endIndex) ? value : item)
+    const replaceValue: Array<T | F> = []
+    initialValue.forEach((item: T, index) => {
+        replaceValue.push(
+            index >= startIndex && index <= endIndex ? value : item
+        )
     })
     return replaceValue
 }
